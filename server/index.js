dfb5e1eb9437d157023a44ac6ce286341ee8aab3
kennykha <kennykha@kkha.lan>
2021-05-14 12:40:31 -0700

    Adding express backend proxy to react app

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const App = express();
+
+// App.use(express.static(path.join(__dirname, '../', '/build')))
+// App.use(express.static(path.join(__dirname, '../', '/public')))
+App.use(express.json());
+
+App.get('/', (req, res) => {
+    // console.log(path.join(__dirname, '../', '/public/index.html'));
+    // res.sendFile(path.join(__dirname, '../public', 'index.html'));
+    res.status(200).send('Root end point hit');
+})
+
+App.get('/test', (req,res) => {
+    console.log('hello')
+    res.status(200).json({test: 1});
+})
+
+App.listen(3001, () => {
+    console.log('Listening on Port 3001')
+})
