2210e6c7a7201b46eb08316d36f2013f36eae0ce
kennykha <kennykha@kkha.lan>
2021-05-18 18:25:50 -0700

    Added dynamic routing path for users

--- src/components/App.js
+++ src/components/App.js
@@ -15,7 +15,7 @@ function App() {
           <Route exact path='/'>
             <User />
           </Route>
-          <Route path='/user/Kenny'>
+          <Route path='/user/:name'>
             <UserData />
           </Route>
         </main>
