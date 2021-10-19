01fd796254c378e4de8d73c4dd8a2e2e00f019a1
kasperkuo <kasper.kuo@gmail.com>
2021-10-19 15:07:59 -0700

    fix lint issues

--- frontend/src/pages/UsersListPage.js
+++ frontend/src/pages/UsersListPage.js
@@ -1,18 +1,16 @@
-// import React, { useState, useEffect } from "react";
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { getUsers } from "../actions/users";
 import UsersList from "../components/usersList/UsersList";
 import EditForm from "../components/usersList/EditForm";
-import { users } from "../mocks/test";
 
 export default function UsersPage(props) {
-  // const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState([]);
 
   useEffect(() => {
     getUsers()
       .then((response) => {
         console.log("Response from server/db: ", response.data);
-        // setUsers(response.data);
+        setUsers(response.data);
       })
       .catch((err) => console.log(err));
   }, []);
@@ -20,8 +18,7 @@ export default function UsersPage(props) {
   return (
     <div>
       <UsersList users={users} />
-      <EditForm />
-      {/* <EditForm setUsers={setUsers} /> */}
+      <EditForm setUsers={setUsers} />
     </div>
   );
 }
