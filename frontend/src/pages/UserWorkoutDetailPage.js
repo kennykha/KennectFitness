31131925c3e9dc76bb25cd915f76913eb415fcaf
Kenny Kha <69876050+kennykha@users.noreply.github.com>
2021-10-25 09:24:39 -0500

    Updating SQL schema, brdecoupling workout_reps for weight and reps. Added Moment library for date formatting

--- frontend/src/pages/UserWorkoutDetailPage.js
+++ frontend/src/pages/UserWorkoutDetailPage.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { getUserWorkoutData } from "../actions/users";
+import moment from "moment";
 
 const UserWorkoutDetailPage = (props) => {
   const [workoutData, handelWorkoutData] = useState([]);
@@ -14,11 +15,31 @@ const UserWorkoutDetailPage = (props) => {
         console.log(err);
       });
   }, [name, workout]);
+
+  console.log("WORKOUTDATA", workoutData);
+
   return (
     <div>
-      {workoutData.map((data) => (
-        <div key={data.id}>Hi</div>
-      ))}
+      {workoutData.map((data) => {
+        console.log(moment(data.workoutDate).format("L"));
+        return (
+          <div
+            key={data.id}
+            style={{
+              border: "1px solid black",
+              margin: "2px",
+              width: "600px",
+              display: "flex",
+              justifyContent: "space-between",
+            }}
+          >
+            <div>Date: {moment(data.workoutDate).format("L")}</div>
+            <div>Workout: {data.workout}</div>
+            <div>Set: {data.current_set}</div>
+            <div>Reps: {data.rep_info}</div>
+          </div>
+        );
+      })}
     </div>
   );
 };
